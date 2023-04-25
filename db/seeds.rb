e74f0fc5a86b12dac6e639de2fdfb1ac6c69abfb
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-25 15:01:40 -0400

    added seed code for Category table. learned how to drop DB with bundle exec rake db:drop:all then bundle exec rake db:migrate db:seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,20 @@ CSV.foreach("db/seed_countries.csv", headers: true) do |row|
 end
 
 CSV.foreach("db/seed_categories.csv", headers: true) do |row|
-  country = Country.new
-  country.name = row["name"]
-  country.save
+  category = Category.new
+  category.name = row["name"]
+  category.save
 end
+
+# CSV.foreach("db/seed_categories.csv", headers: true) do |row|
+#   article = Article.new
+#   article.title = row["title"]
+#   article.published = row["published"]
+#   article.link = row["link"]
+#   article.country_id = row["country_id"]
+#   article.category_id = row["category_id"]
+#   article.save
+# end
 # Seed your database here
 # using ruby i can parse it as a csv
 # loop through file and Model.Create a record for everyline
