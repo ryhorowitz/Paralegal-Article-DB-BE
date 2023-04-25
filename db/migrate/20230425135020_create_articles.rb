970ee133eff1efcee92a9170ac87041bd8ec85ab
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-25 10:08:23 -0400

    added migration files for tables: countries and categories.

--- db/migrate/20230425135020_create_articles.rb
+++ db/migrate/20230425135020_create_articles.rb
@@ -1,4 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
+    create_table :articles do |t|
+      t.string :title
+      t.date :published
+      t.string :link
+      t.integer :country_id
+      t.integer :category_id
+      t.timestamp
+    end
   end
 end
