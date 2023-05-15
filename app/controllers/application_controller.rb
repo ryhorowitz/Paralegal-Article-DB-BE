f4a8d2d6edd85802194ae381889c47a0dea8e938
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-05-15 16:46:53 -0400

    deleted some controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,28 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
-  def getArticles
-    articles = Article.all
-    articles.to_json(include: [{ category: { only: :name } }, { country: { only: :name } }])
-  end
-
-  get "/articles" do
-    getArticles
-  end
-
   get "/countries" do
     countries = Country.all
     countries.to_json(include: :articles)
   end
 
-  get "/categories" do
-    categories = Category.all
-    categories.to_json(include: :articles)
-  end
-
-  get "/articles/country/category" do
-    articles = Article.where(["country_id = '%s' and category_id = '%s'", params[:country_id], params[:category_id]])
-    # binding.pry
-    articles.to_json
-  end
-
   post "/new_article" do
-    article = Article.create(params)
-    getArticles
+    country = Country.find(params[:country_id])
+
+    article = country.articles.create(
+      title: params[:title],
+      link: params[:link],
+      published: params[:published],
+      category_id: params[:category_id],
+    )
+    # article = Article.create(params)
+
+    # return newly created article to_json
   end
 
   post "/country" do
     country = Country.create(params)
+    # send back new country with :articles
     countries = Country.all
     countries.to_json(include: :articles)
   end
@@ -40,11 +30,11 @@ class ApplicationController < Sinatra::Base
   delete "/article/:id" do
     article = Article.find(params[:id])
     article.destroy
-    getArticles
   end
 
   patch "/article/:id" do
     article = Article.find(params[:id])
+
     article.update(
       title: params[:title],
       link: params[:link],
@@ -52,6 +42,5 @@ class ApplicationController < Sinatra::Base
       country_id: params[:country_id],
       category_id: params[:category_id],
     )
-    getArticles
   end
 end
